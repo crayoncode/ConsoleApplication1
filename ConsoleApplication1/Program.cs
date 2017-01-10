b8c13f5e24e7c34289b0af2c780700951ad6872a
Administrator <Administrator@SC-201612241043>
2017-01-10 21:27:35 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = "Data Source=(local);Initial Catalog=Student;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connection);
+            //SqlCommand command = conn.CreateCommand();
+            //command.CommandText = "select * from Person";
+            //conn.Open();
+            //command.ExecuteNonQuery();
+            //conn.Close();
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select * from Person", conn);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+
+            conn.Close();
+            Console.ReadKey();
+        }
+    }
+}
